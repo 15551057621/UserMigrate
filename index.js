93654c71fe562b2c57328f91a3a3fe7fb4da129a
Ice_Rink <15551057621@163.com>
2026-03-28 04:53:43 +0000

    add mc

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
 //LiteLoaderScript Dev Helper
 /// <reference path="/root/VSCode/Library/JS/index.d.ts" /> 
 
+const userMigrate = {
+    mc: (oldName, newName) => {
+        const oldUuld = data.name2uuid(oldName);
+        const newUuld = data.name2uuid(newName);
 
+        if (!oldUuld && !newUuld) return;
+
+        // === NBT === //
+        const oldNbt = mc.getPlayerNbt(oldUuld);
+        mc.setPlayerNbt(oldUuld, mc.getPlayerNbt(newUuld));
+        mc.setPlayerNbt(newUuld, oldNbt);
+
+        // === 积分榜 === //
+        mc.getAllScoreObjectives().forEach(sc => {
+            const oldSc = mc.getPlayerScore(oldUuld, sc.name);
+            mc.setPlayerScore(oldUuld, sc.name, mc.getPlayerScore(newUuld, sc.name))
+            mc.setPlayerScore(newUuld, sc.name, oldSc);
+        });
+        
+    },
+    iland: (oldName, newName) => {
+        if (!ll.hasExported("ILAPI_PosGetLand")) return;
+
+        const oldUuld = data.name2uuid(oldName);
+        const newUuld = data.name2uuid(newName);
+
+
+    }
+}
